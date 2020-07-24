b0fd05f5c09f8a0f6e4f9686ef0a3e8b9147d9d4
Mayur Agarwal <mr.mayurrrr@gmail.com>
2020-07-24 17:59:28 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hiveproj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hiveproj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hiveproj/models/contact.dart';
+import 'contact_page.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(ContactAdapter());
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hive Tutorial',
+      home: FutureBuilder(
+        future: Hive.openBox('contacts',
+            compactionStrategy: (int total, int deleted) {
+          return deleted > 20;
+        }),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError)
+              return Text(snapshot.error.toString());
+            else
+              return ContactPage();
+          } else
+            return Scaffold();
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    Hive.box('contacts').compact();
+    Hive.close();
+    super.dispose();
+  }
+}
